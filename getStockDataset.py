b199ea099d57d3b13490b6a9b94b3c384fd232c0
valentine620721 <78589219+valentine620721@users.noreply.github.com>
2023-03-01 14:23:18 +0800

    Update getStockDataset.py

--- getStockDataset.py
+++ getStockDataset.py
@@ -1,25 +1,24 @@
 import cx_Oracle
 
 
-class getOracleDataset:
-    def __init__(self,hostname,port,servicename,id,passwd,sqlstr):
+class getDataset:
+    def __init__(self,hostname,port,servicename,id,passwd,sqlstr,stockid):
         pass
 
-    def getOracleDataset(hostname,port,servicename,id,passwd,sqlstr):
+    def getOracleDataset(hostname,port,servicename,id,passwd,sqlstr,stockid):
         try:
             # 建立與Oracle數據庫的連接
             dsn = cx_Oracle.makedsn(host = hostname, port = port, service_name = servicename) 
             conn = cx_Oracle.connect(user=id, password=passwd, dsn = dsn)
             # 創建游標對象
             cursor = conn.cursor()
-            cursor.execute(sqlstr)
+            cursor.execute(sqlstr,{"stockid":stockid})    ## 查詢條件使用參數
             # 獲取數據集
             dataset = cursor.fetchall()
+            return dataset
             # 關閉游標和連接
             cursor.close()
             conn.close()
-            return dataset
-            print("Connect to Oracle Database successful.")
 
-        except:
-            print('Connect to Oracle Database error! Failed  connect to oracle database...')
+        except cx_Oracle.DatabaseError as error:
+            print('Database Error:',error)
